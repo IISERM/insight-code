29cfc0feb1dd92cfb663906c68a7bada9b332154
PsuedoFractal <54537673+PsuedoFractal@users.noreply.github.com>
2023-08-19 20:29:48 +0530

    Partial google signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:insight/screens/loginPage/loginPage.dart';
+
+import 'firebase_options.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const InsightApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InsightApp extends StatelessWidget {
+  const InsightApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Insight',
+      title: "Insight",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Container(), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const LoginPage()
     );
   }
 }
--- lib/screens/loginPage/widgets/studentSignInButton.dart
+++ lib/screens/loginPage/widgets/studentSignInButton.dart
@@ -1,16 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:insight/common/backend/auth/authentication.dart';
 
-class GoogleSignInButton extends StatelessWidget {
-  const GoogleSignInButton({super.key});
+class StudentSignInButton extends StatefulWidget {
+  const StudentSignInButton({super.key});
+
+  @override
+  State<StudentSignInButton> createState() => _StudentSignInButtonState();
+}
+
+class _StudentSignInButtonState extends State<StudentSignInButton> {
+
+  bool _isSigningIn = false;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: (){
+    return Padding(
+      padding: const EdgeInsets.only(top: 50),
+      child: _isSigningIn
+          ? const CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+      )
+          : OutlinedButton(
+               style: ButtonStyle(
+                backgroundColor: MaterialStateColor.resolveWith((states) => Colors.white),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+              ),
+              onPressed: () async {
 
-      },
-      style:
-        ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => Colors.white)),
-      child: const Text("Sign in with Google"),);
+                setState(() {
+                  _isSigningIn = true;
+                });
+
+                User? user = await Authentication.signInWithGoogle(context: context);
+                if (user != null) {
+                  print("User is not null");
+                  print(user);
+                }
+                else {
+                  print("User is null");
+                }
+              },
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Sign in with Google',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+          ),
+    );
   }
 }
+
