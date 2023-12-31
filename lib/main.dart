3fa25b916b6298d05a6524cdfb6e98ae5b769c56
PsuedoFractal <kshitishkumarratha@gmail.com>
2023-12-31 13:01:41 +0530

    ♻️ Base Folder Structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'firebase_options.dart';
+import 'config/firebase_options.dart';
 
 void main() {
   runApp(
